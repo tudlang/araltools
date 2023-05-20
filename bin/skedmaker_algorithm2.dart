f293f0b9c2a660e622335129a0a3593a02e956f8
Yivan000 <69418786+Yivan000@users.noreply.github.com>
2023-05-20 14:46:37 +0800

    Created binary conflict detector

--- bin/skedmaker_algorithm2.dart
+++ bin/skedmaker_algorithm2.dart
@@ -1,9 +1,13 @@
 import 'dart:convert';
+import 'dart:math';
+import 'dart:typed_data';
 
 import 'package:araltools/araltools/skedmaker/classes.dart' hide ScheduleWeek;
 import 'package:araltools/araltools/skedmaker/functions.dart';
 import 'package:araltools/araltools/skedmaker/test_tables.dart';
 import 'package:collection/collection.dart';
+
+int factorial(int n) => n == 0 ? 1 : n * factorial(n - 1);
 
 void main() {
   // count execution time
@@ -13,13 +17,26 @@ void main() {
 
   subjects['CALENG2'] = parse(caleng2);
   subjects['LBYMF1C'] = parse(lbymf1c);
-  //subjects['LCLSONE'] = parse(lclsone);
+  subjects['LCLSONE'] = parse(lclsone);
   //subjects['LBYMF1D'] = parse(lbymf1d);
 
   //List<Offering> all = [for (final i in subjects.values) ...i];
+  
+  while (subjects.values.any((element) => element.isNotEmpty)){
+    final week = ScheduleWeek();
+    final _subjects = Map.of(subjects);
 
-
-
+    for (final entry in  subjects.keys){
+      try {
+        week.add(_subjects[entry]!.last);
+        _subjects[entry]!.removeLast();
+      } catch (e){
+        continue;
+      }
+    }
+    
+    print(week.daysOfferingsString);
+  }
 
   stopwatch.stop();
   print("ELAPSED TIME: ${stopwatch.elapsedMilliseconds}");
@@ -29,7 +46,73 @@ void main() {
 
 final days = ["M", "T", "W", "H", "F", "S"];
 
-class ScheduleWeek{
+class ScheduleWeek {
+  Map<int, BigInt> daysBytes;
+  Map<int, List<Offering>> daysOfferings;
+  Set<(String, int)> subjects;
 
+  ScheduleWeek()
+      : daysBytes = {
+          0: BigInt.zero,
+          1: BigInt.zero,
+          2: BigInt.zero,
+          3: BigInt.zero,
+          4: BigInt.zero,
+          5: BigInt.zero,
+        },
+        daysOfferings = {
+          0: [],
+          1: [],
+          2: [],
+          3: [],
+          4: [],
+          5: [],
+        }, subjects={};
 
+  BigInt toByte(int start, int end) =>
+      BigInt.from(2).pow(end - start) - BigInt.one << start;
+
+  bool isByteConflicting(BigInt a, BigInt b) => a & b != BigInt.zero;
+
+  addByte({required int daycode, required int start, required int end}) {
+    final toAdd = toByte(start, end);
+    final byteOfDay = daysBytes[daycode]!;
+
+    if (isByteConflicting(byteOfDay, toAdd)) {
+      throw Error();
+    }
+
+    daysBytes[daycode] = byteOfDay | toAdd;
+  }
+
+  add(Offering offering) {
+    final start = offering.scheduleTimeStart;
+    final end = offering.scheduleTimeEnd;
+
+    int dayFromCode(String code) => switch (code) {
+          'M' => 0,
+          'T' => 1,
+          'W' => 2,
+          'H' => 3,
+          'F' => 4,
+          'S' => 5,
+          _ => throw ArgumentError()
+        };
+
+    // make it a for loop so that the multiple days are allowed
+    for (final daycode in offering.scheduleDay.daycode.split('')) {
+      addByte(
+        daycode: dayFromCode(daycode),
+        start: start,
+        end: end,
+      );
+      daysOfferings[dayFromCode(daycode)]!.add(offering);
+    }
+
+  }
+
+  String get daysOfferingsString => JsonEncoder.withIndent("  ").convert(
+      daysOfferings.map((key, value) => MapEntry(
+          key.toString() + " ${value.length}",
+          value.map((key) => key.toString()).toList())));
 }
