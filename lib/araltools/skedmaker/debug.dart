8288a88c6061a88cb11a9abef62380c59ba596f2
Yivan000 <69418786+Yivan000@users.noreply.github.com>
2023-07-18 08:54:50 +0800

    📄 Updated license headers

--- lib/araltools/skedmaker/debug.dart
+++ lib/araltools/skedmaker/debug.dart
@@ -1,3 +1,20 @@
+// Copyright (C) 2023 Tudlang
+// 
+// This file is part of AralTools.
+// 
+// AralTools is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// AralTools is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with AralTools.  If not, see <http://www.gnu.org/licenses/>.
+
 import 'package:flutter/material.dart';
 
 import 'classes.dart';
